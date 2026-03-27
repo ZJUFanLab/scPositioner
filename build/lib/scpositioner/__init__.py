07cde190d8ac8cabbcb85162bc5ca696b7fca5a7
ProDong0512 <104128192+ProDong0512@users.noreply.github.com>
2026-03-27 20:04:32 +0800

    Update __init__.py

--- build/lib/scpositioner/__init__.py
+++ build/lib/scpositioner/__init__.py
@@ -1,15 +1,13 @@
 import sys
 from importlib.metadata import version
 
-from . import datasets
 from . import plot as pl
-from . import preprocess as pp
 from . import tools as tl
 from . import analysis as al
 
 # has to be done at the end, after everything has been imported
-sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["tl", "pp", "pl", "al"]})
+sys.modules.update({f"{__name__}.{m}": globals()[m] for m in ["tl", "pl", "al"]})
 
 __version__ = version("scpositioner")
 
-__all__ = ["__version__", "datasets", "tl", "pp", "pl", "al"]
+__all__ = ["__version__", "tl", "pl", "al"]
